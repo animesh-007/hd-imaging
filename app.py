d735ac322d4fca76ef683e5ab7c8b9d390bcda3b
animesh-007 <animeshagarwal7@gmail.com>
2020-02-23 14:56:44 +0530

    able to combine image now

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import glob
 import jsonify
 import numpy as np
 import json
+from PIL import Image
 
 
 __author__ = 'animesh'
@@ -62,10 +63,9 @@ def upload():
 
         
     # print(g.tolist())
-    r = r.tolist()
-    return json.dumps(r)
-    b = b.tolist()
-    g = g.tolist()
+    #r = r.tolist()
+    #b = b.tolist()
+    #g = g.tolist()
     # return send_from_directory("images", filename, as_attachment=True)
     # return render_template("complete.html", image_name=filename)
     
@@ -81,6 +81,65 @@ def upload():
 
 
 # #print(image)
+    image=Image.open("images/rgb01.png")
+
+    #print(image.size) #size is inverted i.e columns first rows second eg: 500,250
+
+    #convert to array
+    li_r=list(image.getdata(band=0))
+    #arr_r=np.array(li_r,dtype="uint8")
+    li_g=list(image.getdata(band=1))
+    #arr_g=np.array(li_g,dtype="uint8")
+    li_b=list(image.getdata(band=2))
+    #arr_b=np.array(li_b,dtype="uint8")
+
+
+    return json.dumps({'blue': li_r, 'green': li_g, 'red': li_r}, indent=4, sort_keys=True)
+
+
+# reshape 
+# red,green,blue=upload()
+    # reshaper=red.reshape(316,314) #size flipped so it reshapes correctly
+    # reshapeb=green.reshape(316,314)
+    # reshapeg=blue.reshape(316,314)
+
+    
+    # upload.save("/images/merged")
+
+    
+    #merged.show()
+
+    # reshape 
+#pixelDensity(1)
+img = json.loads(upload())
+
+blue=img["blue"]
+arr_b=np.array(blue,dtype="uint8")
+
+red=img["red"]
+arr_r=np.array(red,dtype="uint8")
+
+green=img["green"]
+arr_g=np.array(green,dtype="uint8")
+
+
+reshaper=arr_r.reshape(316,314) #size flipped so it reshapes correctly
+reshapeb=arr_b.reshape(316,314)
+reshapeg=arr_g.reshape(316,314)
+
+imr=Image.fromarray(reshaper,mode=None) # mode I
+imb=Image.fromarray(reshapeb,mode=None)
+img=Image.fromarray(reshapeg,mode=None)
+
+merged=Image.merge("RGB",(imr,img,imb))
+
+#red and blue colour are not coming properly
+merged=merged.save("combinedimage.jpg")
+
+
+
+
+
 
 
 @app.route('/upload/<filename>')
