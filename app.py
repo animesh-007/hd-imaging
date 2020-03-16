b778424efb5907ad74e1af3a441ce98b811e1ba9
animesh-007 <animeshagarwal7@gmail.com>
2020-03-17 00:09:34 +0530

    uploaded new files

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def upload():
 # # #print(image)
     image_path = "images/image.jpg"
     image = cv.imread(image_path, 1)
-    # image=Image.open("images/image.jpg")
+    
+    # splitting image into 3 channels
     b = image[:,:,0]
     g = image[:,:,1]
     r = image[:,:,2]
@@ -53,6 +54,8 @@ def upload():
     cv.imwrite("templates/images/splitedcomponents/imageblue.jpg", b)
     # r=r.save("images/r_image.jpg")
 
+
+    # combining channels into one
     a = np.ones(b.shape, dtype=b.dtype)*50
     bg_merge = cv.merge((b,g,a))
     gr_merge = cv.merge((g,r,a))
@@ -62,7 +65,6 @@ def upload():
     cv.imwrite("templates/images/combinedimages/imagegr.jpg",gr_merge)
     cv.imwrite("templates/images/combinedimages/imagerb.jpg",rb_merge)
     cv.imwrite("templates/images/combinedimages/imagebgr.jpg", bgr_merge)
-    # cv.imwrite("images/bgmerge.jpg", bg_merge)
     
 
     return render_template("complete.html",image_name="image.jpg")
@@ -72,47 +74,7 @@ def upload():
 def send_image(filename):
     return send_from_directory("images","image.jpg")
 
-@app.route("/gallery") 
-def get_gallery():
-    image_names=os.listdir("./images")
-    return render_template("gallery.html",image_names=image_names)   
 
-
-
-
-#     #convert to array
-#     li_r=list(image.getdata(band=0))
-    
-#     li_g=list(image.getdata(band=1))
-    
-#     li_b=list(image.getdata(band=2))
-
-
-#     return json.dumps({'blue': li_b, 'green': li_g, 'red': li_r}, indent=4, sort_keys=True)
-
-# img = json.loads(upload())
-
-# blue=img["blue"]
-# arr_b=np.array(blue,dtype="uint8")
-
-# red=img["red"]
-# arr_r=np.array(red,dtype="uint8")
-
-# green=img["green"]
-# arr_g=np.array(green,dtype="uint8")
-
-
-# reshaper=arr_r.reshape(316,314) #size flipped so it reshapes correctly
-# reshapeb=arr_b.reshape(316,314)
-# reshapeg=arr_g.reshape(316,314)
-
-# imr=Image.fromarray(reshaper,mode=None) # mode I
-# imb=Image.fromarray(reshapeb,mode=None)
-# img=Image.fromarray(reshapeg,mode=None)
-
-# merged=Image.merge("RGB",(imr,img,imb))
-
-# merged=merged.save("combinedimage.jpg")
 
 
 if __name__ == "__main__":
--- readme.txt
+++ readme.txt
@@ -2,13 +2,15 @@ Aim : Creating Server which returns image channels R, G, B separately to client 
 
 software used: 
 1.Flask as framework
-2.<opencv-python> for image handling
+2.opencv-python for image handling
 
-initialising: 
+Initialising: 
 command 'python app.py' to start the server at PORT 4555.
 
-workflow:
+Workflow:
 a web page served at port 4555. pls attach a valid image file and click on channel. the image file saved by sever in a directory at /images.
-when Upload button is clicked image from uploads directory turned into individual channels and are also combined which are shown on next page.
-it uses opencv-python.
+when Upload button is clicked image from uploads directory turned into individual channels and are stored in directory templates/images/splitedcomponents also combined images are also formed which are stored in directory templates/images/combinedimages.
 
+All the operation are done using opencv-python.
+
+Final result are shown using complete.html page
