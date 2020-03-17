eb042f3bdb9e49a52bdfd18f196ee83143417085
animesh-007 <animeshagarwal7@gmail.com>
2020-03-18 03:01:23 +0530

    lycon applied

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template
 import numpy as np
-import cv2 
+import lycon
 
 
 __author__ = 'animesh'
@@ -37,46 +37,19 @@ def upload():
         
 
     image_path = "images/image.jpg"
-    image = cv2.imread(image_path, 1)
+    # image = cv2.imread(image_path, 1)
+    image = lycon.load(image_path)
     
     # splitting image into 3 channels
-    b = image[:,:,0]
+    r = image[:,:,0]
     g = image[:,:,1]
-    r = image[:,:,2]
+    b = image[:,:,2]
 
-    cv2.imwrite("templates/images/splitedcomponents/imagered.jpg", r)
-    cv2.imwrite("templates/images/splitedcomponents/imagegreen.jpg", g)
-    cv2.imwrite("templates/images/splitedcomponents/imageblue.jpg", b)
-    
-
-    # combining R,G,B channels into one
-    a = np.ones(b.shape, dtype=b.dtype)*50
-    bgr_merge = cv2.merge((b,g,r,a))
-    
-    #setting red channel of combine image 0 for viewing
-    bg = bgr_merge.copy()
-    bg[:,:,2]=0
-    
-    #setting blue channel of combine image 0 for viewing
-    gr = bgr_merge.copy()
-    gr[:,:,0]=0
-    
-
-    #setting green channel of combine image 0 for viewing
-    rb = bgr_merge.copy()
-    rb[:,:,1]=0
-
-    cv2.imwrite("templates/images/combinedimages/imagebgr.jpg", bgr_merge)
-    cv2.imwrite("templates/images/combinedimages/imagebg.jpg",bg)
-    cv2.imwrite("templates/images/combinedimages/imagegr.jpg",gr)
-    cv2.imwrite("templates/images/combinedimages/imagerb.jpg",rb)
-    
-    
-
-    return render_template("complete.html")
+    return np.array_str(np.array([r,g,b]))
 
 
-
+# combining R,G,B channels into one
+combinedimaged = np.dstack((r,g,b))
 
 
 if __name__ == "__main__":
